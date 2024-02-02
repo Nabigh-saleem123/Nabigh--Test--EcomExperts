8d83c212c5df81060bd512aacf99c8944dbba622
unknown <nabighsaleem1234@outlook.com>
2024-02-03 00:21:05 +0500

    Updating select option of variant

---
+++ assets/bunlde-offer.js
@@ -0,0 +1,32 @@
+function handleVariantSelectOptions(){
+    // Function to handle variant option to pick and diable
+    var selectInitialValue = document.getElementById('Option-template--15157697052758__main-1').value;
+    var addToCartButton = document.getElementById('ProductSubmitButton-template--15157697052758__main');
+    var buyButton = document.querySelector('shopify-payment-button__button');
+    // Checking the option and making sure if Unselected is the value
+    if (selectInitialValue === "Unselected") {
+        addToCartButton.style.pointerEvents = "none"; 
+        addToCartButton.style.opacity = "0.6"; 
+        buyButton.style.pointerEvents = "none"; 
+        buyButton.style.opacity = "0.6"; 
+    } else {
+        addToCartButton.style.pointerEvents = "auto"; // Enable interactions
+        addToCartButton.style.opacity = "1"; // Restore opacity
+        buyButton.style.pointerEvents = "auto"; // Enable interactions for the payment button
+        buyButton.style.opacity = "1"; // Restore opacity for the payment button
+    }
+  }
+  
+  // Attach the function to DOM ready event
+  document.addEventListener('DOMContentLoaded', function () {
+    // Call the function on DOM ready
+    handleOptionChange();
+  
+    // Add a change event listener to the select element
+    var selectedOption = document.getElementById('Option-template--22058985128211__main-1');
+    selectedOption.addEventListener("change", handleOptionChange);
+    
+    // Add setTimeout to call the function after 1 second
+    setTimeout(handleOptionChange, 1000);
+  });
+  
