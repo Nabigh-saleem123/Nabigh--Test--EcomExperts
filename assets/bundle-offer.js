ddb8117a1cc2ffab8fd97ec94e033f663366fd0b
unknown <nabighsaleem1234@outlook.com>
2024-02-03 22:20:53 +0500

    Latest after solving issues

--- assets/bundle-offer.js
+++ assets/bundle-offer.js
@@ -1,6 +1,6 @@
 function handleVariantSelectOptions() {
   // Function to handle variant option to pick and diable
-  var selectInitialValue = document.getElementById('Option-template--15157697052758__main-1').value;
+  var selectInitialValue = document.getElementById('custom-size').value;
   var addToCartButton = document.querySelector('.product-form__submit');
   var buyButton = document.querySelector('.shopify-payment-button__button');
   // Checking the option and making sure if Unselected is the value
@@ -19,13 +19,13 @@ function handleVariantSelectOptions() {
 
 // Attach the function to DOM ready event
 document.addEventListener('DOMContentLoaded', function () {
-  var selectElementOnLoad = document.getElementById('Option-template--15157697052758__main-1');
+  var selectElementOnLoad = document.getElementById('custom-size');
   selectElementOnLoad.value = 'Unselected';
   // Call the function on DOM ready
   handleVariantSelectOptions();
 
   // Add a change event listener to the select element
-  var selectedOption = document.getElementById('Option-template--15157697052758__main-1');
+  var selectedOption = document.getElementById('custom-size');
   selectedOption.addEventListener("change", handleVariantSelectOptions);
 
   // Add setTimeout to call the function after 1 second
@@ -36,46 +36,56 @@ document.addEventListener('DOMContentLoaded', function () {
 document.addEventListener('DOMContentLoaded', function () {
   const TARGET_VARIANT_ID = '40631393058902'; // Variant id of Tan & Medium
   const SOFT_WINTER_JACKET = '40621519306838'; // Variant ID of the "Soft Winter Jacket"
- 
-  document.querySelectorAll('.product-form__submit').forEach(button => {
-    button.addEventListener('click', function (event) {
-      // Check cart contents before adding the additional product
-      fetch('/cart.js')
-        .then(response => response.json())
-        .then(cart => {
-          const isBeingAdded = this.closest('form').querySelector('[name="id"]').value === TARGET_VARIANT_ID;
 
-          // If the target product variant is the one being added, add the jacket
-          if (isBeingAdded) {
-            let formData = {
-              'items': [{
-                'id': SOFT_WINTER_JACKET,
-                'quantity': 1
-              }]
-            };
+  // Add event listener for change event
+  customSizeSelect.addEventListener('change', function () {
+    // Get the selected value
+    var customSizeValue = customSizeSelect.value;
 
-            fetch('/cart/add.js', {
-              method: 'POST',
-              headers: {
-                'Content-Type': 'application/json'
-              },
-              body: JSON.stringify(formData)
-            })
-              .then(response => {
-                if (!response.ok) throw new Error(`HTTP error! Status: ${response.status}`);
-                return response.json(); // Parse the JSON of the response.
-              })
-              .then(data => {
-                console.log('Jacket added to cart:', data);
-              })
-              .catch(error => {
-                console.error('Error adding jacket to cart:', error);
-              });
-          }
+    if (customSizeValue === "Medium") {
+      // Attach event listener to buttons
+      document.querySelectorAll('.product-form__submit').forEach(button => {
+        button.addEventListener('click', function (event) {
+          // Check cart contents before adding the additional product
+          fetch('/cart.js')
+            .then(response => response.json())
+            .then(cart => {
+              const isBeingAdded = this.closest('form').querySelector('[name="id"]').value === TARGET_VARIANT_ID;
+
+              // If the target product variant is the one being added, add the jacket
+              if (isBeingAdded) {
+                let formData = {
+                  'items': [{
+                    'id': SOFT_WINTER_JACKET,
+                    'quantity': 1
+                  }]
+                };
+
+                fetch('/cart/add.js', {
+                  method: 'POST',
+                  headers: {
+                    'Content-Type': 'application/json'
+                  },
+                  body: JSON.stringify(formData)
+                })
+                  .then(response => {
+                    if (!response.ok) throw new Error(`HTTP error! Status: ${response.status}`);
+                    return response.json(); // Parse the JSON of the response.
+                  })
+                  .then(data => {
+                    console.log('Jacket added to cart:', data);
+                  })
+                  .catch(error => {
+                    console.error('Error adding jacket to cart:', error);
+                  });
+              }
+            });
         });
-    });
+      });
+    }
   });
 });
+
 // Removing product if the main product variant is removed from cart
 function removeFromCart(variantIdToRemove, callback) {
   fetch('/cart/change.js', {
